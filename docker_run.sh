74e1015e2d32aa8f4ad41d1bb4da11698aac412a
scucoco <scucoco@users.noreply.github.com>
2019-01-21 21:08:01 +0800

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -5,9 +5,9 @@ set -e
 exec ss-server -v \
     -s ${SHADOWSOCKS_HOST:-0.0.0.0} \
     -p ${SHADOWSOCKS_PORT:-8388} \
-    -k ${SHADOWSOCKS_SCERET:-bob180180180} \
+    -k ${SHADOWSOCKS_SCERET:-801201} \
     -t ${SHADOWSOCKS_TIMEOUT:-300} \
-    -m ${SHADOWSOCKS_ENCRYPTIONMETHOD:-chacha20} \
+    -m ${SHADOWSOCKS_ENCRYPTIONMETHOD:-aes-256-cfb} \
     -n ${SHADOWSOCKS_MAXOPENFILES:-1000} \
     --fast-open -u \
     --plugin obfs-server --plugin-opts obfs=${SIMPLE_OBFS_METHOD:http}
